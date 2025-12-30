56d4357f577192bdeac7a38fd289383931ebb2ae
Hrithivk <hrithivkgo1111@gmail.com>
2025-12-31 00:17:14 +0530

    adding p

--- complete/app.py
+++ complete/app.py
@@ -1,6 +1,7 @@
 from dash import Dash, html, page_container
 
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
+server = app.server
 
 app.layout = html.Div([
     page_container
